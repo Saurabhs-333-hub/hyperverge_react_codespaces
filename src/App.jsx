faa020358e9125a7e90c69f696b6d3cec125ba96
Saurabh Soni <90748697+Saurabhs-333-hub@users.noreply.github.com>
2023-06-29 13:33:35 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <Helmet>
-      <script src="https://hv-camera-web-sg.s3-ap-southeast-1.amazonaws.com/hyperverge-web-sdk@5.1.6/src/sdk.min.js"></script>
+      <script crossorigin src="https://hv-camera-web-sg.s3-ap-southeast-1.amazonaws.com/hyperverge-web-sdk@5.1.6/src/sdk.min.js"></script>
       </Helmet>
       <Home/>
     </div>
