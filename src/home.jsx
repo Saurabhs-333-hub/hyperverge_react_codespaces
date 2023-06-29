996906df8a18fd86bd54a3cdf4295c5df14569c4
Saurabh Soni <90748697+Saurabhs-333-hub@users.noreply.github.com>
2023-06-29 16:51:57 +0000

    commit

--- src/home.jsx
+++ src/home.jsx
@@ -1,40 +1,35 @@
 import React from "react";
 function Home() {
     function startKYC() {
-      // const accessToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6InRzNmhycSIsImhhc2giOiI2YTllZTY4YTMxZjA3NzgxOGUxODdiNzhhZGFmYTIwZWVhZjZiMDUzNzY2MjdlMjU1OWQ0Yzg2YTJkYjY2MjEyIiwiaWF0IjoxNjg0ODM3NzQ1LCJleHAiOjE2ODQ4MzgwNDUsImp0aSI6ImUxNzk4ZmQyLTYxYzEtNDUzZS05MzBmLTg0MjI1Mjg5MmYzZCJ9.d6SM-IkEtMl_4kknmGEWNtjjfrgf-xlrJCVytwcFC-zCPUVuaHx3EJt4FbhnTv8zPfC3NJenrXy3n4vctJjNi4YpfVOQsv6wGquB4mv-4ahJBCd6GAsIBsBmeuuoeqoUJRgSPi71lu_S_ofr5O0wp6U1yf5s7YlCFLYQKEZ9Qus"
-      // const hyperKycConfig = new HyperKycConfig(
-      //     accessToken,
-      //     "Liveness",
-      //     "1234"
-      // );
-      // HyperKYCModule.launch(hyperKycConfig, handler);
-        window.HyperSnapSDK.init('eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6InRzNmhycSIsImhhc2giOiI2YTllZTY4YTMxZjA3NzgxOGUxODdiNzhhZGFmYTIwZWVhZjZiMDUzNzY2MjdlMjU1OWQ0Yzg2YTJkYjY2MjEyIiwiaWF0IjoxNjg0ODM3NzQ1LCJleHAiOjE2ODQ4MzgwNDUsImp0aSI6ImUxNzk4ZmQyLTYxYzEtNDUzZS05MzBmLTg0MjI1Mjg5MmYzZCJ9.d6SM-IkEtMl_4kknmGEWNtjjfrgf-xlrJCVytwcFC-zCPUVuaHx3EJt4FbhnTv8zPfC3NJenrXy3n4vctJjNi4YpfVOQsv6wGquB4mv-4ahJBCd6GAsIBsBmeuuoeqoUJRgSPi71lu_S_ofr5O0wp6U1yf5s7YlCFLYQKEZ9Qus', window.HyperSnapParams.Region.AsiaPacific);
-        window.HyperSnapSDK.startUserSession();
-        const hvDocConfig = new window.HVDocConfig();
-        hvDocConfig.setOCRDetails('https://vnm-docs.hyperverge.co/v2/nationalID',                                                                hvDocConfig.DocumentSide.FRONT, {}, {});
-        hvDocConfig.setShouldShowInstructionPage(true);
-       
-        const callback = (HVError, HVResponse) => {
-              if (HVError) {
-                var para = document.createElement('p');
-                var node = document.createTextNode(JSON.stringify(HVError));
-                para.appendChild(node);
-                var element = document.getElementById('div1');
-                element.appendChild(para);
-              } else {
-                var para = document.createElement('p');
-                var node = document.createTextNode(JSON.stringify(HVResponse));
-                para.appendChild(node);
-                var element = document.getElementById('div1');
-                element.appendChild(para);
-              }
-        };
-        window.HVDocsModule.start(hvDocConfig, callback);
+      const accessToken="Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6InRzNmhycSIsImhhc2giOiI2YTllZTY4YTMxZjA3NzgxOGUxODdiNzhhZGFmYTIwZWVhZjZiMDUzNzY2MjdlMjU1OWQ0Yzg2YTJkYjY2MjEyIiwiaWF0IjoxNjg4MDU2NDMyLCJleHAiOjE2ODgwNTY3MzIsImp0aSI6ImQ2OGVkOThlLWRkM2EtNDk5NC04YTBhLTA2OTQ5NmJjMjk0MiJ9.uC283ofOD5Ev0o4Uwb0BHURVdoXLi0K3mIq-oG1DmCIj_CZ-GxzqH5AB4bzDyzHJ5XfmXhLc7cX3cvvgnWcQ2hwZC6cpWCVVRu7PCxn8a-JNqkEwvhsGyKT8lYulVUyOBKL1fQ0K3gXeeZpc5HQVVh4yKAvQwsPn6C49-64SgyM";
+      const workflowId="Liveness";
+      const hyperKycConfig = new window.HyperKycConfig(accessToken, workflowId, "1234");
+      const handler = (HyperKycResult) => {
+        switch(HyperKycResult.status) {
+          case "user_cancelled":
+              // user cancelled
+              alert("User Cancelled!")
+          break;
+          case "error":
+            // failure
+            alert("Error")
+          break;
+          case "auto_approved":
+          case "auto_declined":
+          case "needs_review":
+            // workflow success
+            alert("Workflow Success!")
+          break;
+          default:
+            console.log("success")
+            }
+    }
+    window.HyperKYCModule.launch(hyperKycConfig, handler);
       }
   return (
     <div>
 <button onClick={startKYC}>
-  Say Hello
+  Start KYC
 </button>
     </div>
   );
